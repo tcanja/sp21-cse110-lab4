4c5e71b6b71a36d338c92188c67e4f05388cf0a3
tcanja <47432895+tcanja@users.noreply.github.com>
2021-04-26 22:00:56 -0700

    creating part1b-question16

---
+++ part1/part1b-question16.js
@@ -0,0 +1,15 @@
+let statistics = {
+   redCars: 21,
+   blueCars: 45,
+   greenCars: 12,
+   raceCars: 5,
+   blackCars: 40,
+   rareCars: 2
+};
+
+
+for (const i in statistics) {
+   if (i.startsWith('r') || (statistics[i] % 2) === 1) {
+      console.log(statistics[i]);
+   }
+}
